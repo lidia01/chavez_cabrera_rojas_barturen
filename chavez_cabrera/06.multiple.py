137143d1860dc03a0f61d01b38ca406bb55564e5
lidia <lchavezca@unprg.edu.pe>
2019-12-09 02:54:32 -0500

    se agrego 06.multiple.py

---
+++ chavez_cabrera/06.multiple.py
@@ -0,0 +1,16 @@
+#programa 06
+import os
+#declara
+nombre_del_solicitante=""
+ano_de_inscripcion=0
+#Input via os
+nombre_del_solicitante=(os.sys.argv[1])
+ano_de_inscripcion=int(os.sys.argv[2])
+
+#processing
+#si el numero de hojas supera a 35
+#mostrar "gratis anillado"
+if(ano_de_inscripcion<2030):
+    print("no pagan inscripcion")
+if(ano_de_inscripcion>=2019):
+    print("si pagan inscripcion")
