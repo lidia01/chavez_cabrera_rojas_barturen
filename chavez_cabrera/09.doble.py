0e555d4bf8f3a46a4965b4e590cec46ca9e44bf8
lidia <lchavezca@unprg.edu.pe>
2019-12-09 02:58:18 -0500

    se agrego 08.doble.py

---
+++ chavez_cabrera/09.doble.py
@@ -0,0 +1,16 @@
+#programa 09
+import os
+#declara
+nombre=""
+nro_de_cajas=0
+#Input via os
+nombre=(os.sys.argv[1])
+nro_de_cajas=int(os.sys.argv[2])
+
+#processing
+#si el numero de casas supera a 85
+#mostrar "gratis bonificacion de la casa propia"
+if(nro_de_cajas>73):
+    print("de regalo una caja mas")
+else:
+    print("no alcanzo el limite")
