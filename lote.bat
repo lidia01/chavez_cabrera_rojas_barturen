4c97e39a0b35b09ecd6124ea79e375a690de5955
lidia <lchavezca@unprg.edu.pe>
2019-12-09 03:08:36 -0500

    se agrego lote.bat

--- lote.bat
+++ lote.bat
@@ -117,3 +117,125 @@ python chavez_cabrera/09.multiple.py "san juan de dios" 11
 python chavez_cabrera/10.multiple.py 18 18 20 17
 python chavez_cabrera/10.multiple.py 19 20 20 18
 python chavez_cabrera/10.multiple.py 14 16 17 17
+
+
+python Rojas_Barturen/01.simple.py "elsa" 60
+python Rojas_Barturen/01.simple.py "lidia" 80
+python Rojas_Barturen/01.simple.py "maria" 100
+
+python Rojas_Barturen/02.simple.py "lidia" "avena" 15
+python Rojas_Barturen/02.simple.py "delia" "leche" 50
+python Rojas_Barturen/02.simple.py "ana" "atun" 20
+
+python Rojas_Barturen/03.simple.py "tatiana" 120
+python Rojas_Barturen/03.simple.py "rosmery" 300
+python Rojas_Barturen/03.simple.py "elida" 200
+
+python Rojas_Barturen/04.simple.py "jhair" 100
+python Rojas_Barturen/04.simple.py "yessica" 60
+python Rojas_Barturen/04.simple.py "felicita" 80
+
+python Rojas_Barturen/05.simple.py "july" 10
+python Rojas_Barturen/05.simple.py "pathy" 8
+python Rojas_Barturen/05.simple.py "evelyn" 15
+
+python Rojas_Barturen/06.simple.py "alberto" 15.5
+python Rojas_Barturen/06.simple.py "romina" 19.8
+python Rojas_Barturen/06.simple.py "albert" 15.7
+
+python Rojas_Barturen/07.simple.py "genesis" 17
+python Rojas_Barturen/07.simple.py "gretel" 18
+python Rojas_Barturen/07.simple.py "guisela" 19
+
+python Rojas_Barturen/08.simple.py "maria" 2500
+python Rojas_Barturen/08.simple.py "hilber" 3000
+python Rojas_Barturen/08.simple.py "yanet" 5000
+
+python Rojas_Barturen/09.simple.py "rene" 358.5
+python Rojas_Barturen/09.simple.py "ricardina" 500
+python Rojas_Barturen/09.simple.py "lidia" 800
+
+python Rojas_Barturen/010.simple.py "noe" 150
+python Rojas_Barturen/010.simple.py "didier" 200
+python Rojas_Barturen/010.simple.py "leydy" 600
+
+python Rojas_Barturen/01.doble.py "aldayr" 60
+python Rojas_Barturen/01.doble.py "demetrio" 15
+python Rojas_Barturen/01.doble.py "jhonatan" 80
+
+python Rojas_Barturen/02.doble.py "cythia" "shamphoo" 15
+python Rojas_Barturen/02.doble.py "cythia" "shamphoo" 30
+python Rojas_Barturen/02.doble.py "yanet" "shamphoo" 10
+
+python Rojas_Barturen/03.doble.py "maria" 50
+python Rojas_Barturen/03.doble.py "teodolinda" 40
+python Rojas_Barturen/03.doble.py "lolita" 25
+
+python Rojas_Barturen/04.doble.py "rogelio" 49
+python Rojas_Barturen/04.doble.py "robert" 30
+python Rojas_Barturen/04.doble.py "rogelio" 40
+
+python Rojas_Barturen/05.doble.py "july" 2
+python Rojas_Barturen/05.doble.py "july" 1
+python Rojas_Barturen/05.doble.py "july" 3
+
+python Rojas_Barturen/06.doble.py "clara" 8.5
+python Rojas_Barturen/06.doble.py "clara" 5.12
+python Rojas_Barturen/06.doble.py "clara" 4.3
+
+python Rojas_Barturen/07.doble.py "rosio" 15.5
+python Rojas_Barturen/07.doble.py "pilar" 14.8
+python Rojas_Barturen/07.doble.py "rosio del pilar" 11.4
+
+python Rojas_Barturen/08.doble.py "del pilar" 1200
+python Rojas_Barturen/08.doble.py "elena" 800
+python Rojas_Barturen/08.doble.py "eliana" 500
+
+python Rojas_Barturen/09.doble.py "mary" 250
+python Rojas_Barturen/09.doble.py "malena" 150
+python Rojas_Barturen/09.doble.py "tania" 147
+
+python Rojas_Barturen/010.doble.py "lidia" 69
+python Rojas_Barturen/010.doble.py "marioly" 40
+python Rojas_Barturen/010.doble.py "belen" 36
+
+python Rojas_Barturen/01.multiple.py "karina" 48
+python Rojas_Barturen/01.multiple.py "eliana" 25
+python Rojas_Barturen/01.multiple.py "adaly" 12
+
+python Rojas_Barturen/02.multiple.py "rosio" "atun" 5
+python Rojas_Barturen/02.multiple.py "angela" "avena" 4
+python Rojas_Barturen/02.multiple.py "nicol" "aceite" 7
+
+python Rojas_Barturen/03.multiple.py "daniela" 69
+python Rojas_Barturen/03.multiple.py "carina" 40
+python Rojas_Barturen/03.multiple.py "lidia" 50
+
+python Rojas_Barturen/04.multiple.py "sara" 48
+python Rojas_Barturen/04.multiple.py "lorel" 30
+python Rojas_Barturen/04.multiple.py "sarai" 25
+
+python Rojas_Barturen/05.multiple.py "isolina" 2
+python Rojas_Barturen/05.multiple.py "mabel" 3
+python Rojas_Barturen/05.multiple.py "irma" 1
+
+python Rojas_Barturen/06.multiple.py "claudia" 5.2
+python Rojas_Barturen/06.multiple.py "liliana" 8.8
+python Rojas_Barturen/06.multiple.py "cyndy" 7.7
+
+python Rojas_Barturen/07.multiple.py "rosmery" 15
+python Rojas_Barturen/07.multiple.py "elsa" 13
+python Rojas_Barturen/07.multiple.py "elida" 11
+
+python Rojas_Barturen/08.multiple.py "lidia" 1200
+python Rojas_Barturen/08.multiple.py "maribel" 1508
+python Rojas_Barturen/08.multiple.py "maria" 800
+
+python Rojas_Barturen/09.multiple.py "paola" 200
+python Rojas_Barturen/09.multiple.py "dana" 250
+python Rojas_Barturen/09.multiple.py "diana" 150
+
+python Rojas_Barturen/010.multiple.py "bartolomeo" 69
+python Rojas_Barturen/010.multiple.py "airis" 45
+python Rojas_Barturen/010.multiple.py "kytlhyn" 10
+
