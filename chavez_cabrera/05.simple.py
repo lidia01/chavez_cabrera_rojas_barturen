68262eb7714ea2c79fef51f53be4ef424cc39eca
lidia <lchavezca@unprg.edu.pe>
2019-12-09 02:53:25 -0500

    se agrego 05.simple.py

---
+++ chavez_cabrera/05.simple.py
@@ -0,0 +1,15 @@
+#programa 05
+import os
+#declara
+nro_artisticos=0
+
+#Input via os
+nro_artisticos=int(os.sys.argv[1])
+
+#processing
+#si el numero de artisticos supera a 10 presentaciones culturales
+#mostrar "excelente noche cultural"
+if(nro_artisticos>=10):
+
+#output
+ print("excelente noche cultural")
