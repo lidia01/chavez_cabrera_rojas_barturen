f64a854ced5d9e9f383468388f7e13b9ab0d72ca
lidia <lchavezca@unprg.edu.pe>
2019-12-09 02:52:03 -0500

    se agrego 03.simple.py

---
+++ chavez_cabrera/04.simple.py
@@ -0,0 +1,17 @@
+#programa 04
+import os
+#declara
+nombre=""
+numero_de_paginas=0
+
+#Input via os
+nombre=(os.sys.argv[1])
+numero_de_paginas=int(os.sys.argv[2])
+
+#processing
+#si el numero de paginas supera a 70
+#mostrar "obtienes gratis portada"
+if(numero_de_paginas>70):
+
+#output
+ print(nombre,"obtienes gratis portada")
