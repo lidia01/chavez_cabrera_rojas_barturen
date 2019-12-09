7424da368ffca7aa52445651175e3fa4b8265293
lidia <lchavezca@unprg.edu.pe>
2019-12-09 02:55:55 -0500

    se agrego 07.multiple.py

---
+++ chavez_cabrera/07.multiple.py
@@ -0,0 +1,16 @@
+#programa 07
+import os
+#declara
+direccion=""
+nro_de_casas=0
+#Input via os
+direccion=(os.sys.argv[1])
+nro_de_casas=int(os.sys.argv[2])
+
+#processing
+#si el numero de casas supera a 85
+#mostrar "gratis bonificacion de la casa propia"
+if(nro_de_casas<85):
+    print("gratis bonificacion de la casa propia")
+if(nro_de_casas>=85):
+    print("no alcanza el rango de casas")
