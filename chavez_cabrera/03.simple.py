0679fa374d6f223b74dc24c183c9675bc459ad84
lidia <lchavezca@unprg.edu.pe>
2019-12-09 02:50:48 -0500

    se agrego 03.simple.py

---
+++ chavez_cabrera/03.simple.py
@@ -0,0 +1,14 @@
+#programa 03
+import os
+#Declarar
+Usuario=""
+clave=""
+Usuario=str(os.sys.argv[1])
+clave=str(os.sys.argv[2])
+#processing
+#si la claves es de 4 digitos
+#mostrar "acceso correcto"
+if(clave=="1234"):
+
+#output
+ print("Acceso correcto")
