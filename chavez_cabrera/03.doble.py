d40c7057e8908d5a130b5679006a81020adf4751
lidia <lchavezca@unprg.edu.pe>
2019-12-09 02:49:44 -0500

    se agrego 03.doble.py

---
+++ chavez_cabrera/03.doble.py
@@ -0,0 +1,17 @@
+#programa 03
+import os
+#declara
+nombre=""
+numero_de_paginas=0
+
+#Input via os
+nombre=(os.sys.argv[1])
+numero_de_paginas=int(os.sys.argv[2])
+
+#processing
+#si el numero de paginas supera a 70
+#mostrar "obtienes gratis portada"
+if(numero_de_paginas>70):
+    print(nombre,"obtienes gratis portada")
+else:
+    print(nombre,"no alcanzo la promocion")
