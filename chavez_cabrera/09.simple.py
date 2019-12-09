11c1ef98382eaa384af0678acf6140745ef25670
lidia <lchavezca@unprg.edu.pe>
2019-12-09 02:59:17 -0500

    se agrego 09.simple.py

---
+++ chavez_cabrera/09.simple.py
@@ -0,0 +1,16 @@
+#programa 09
+import os
+#declara
+nombre_del_libro=""
+nro_de_hojas=0
+
+#Input via os
+nombre_del_libro=(os.sys.argv[1])
+nro_de_hojas=int(os.sys.argv[2])
+
+#processing
+#si el numero de hojas supera a 35
+#mostrar "gratis anillado"
+if(nro_de_hojas>20):
+#output
+    print("gratis anillado")
