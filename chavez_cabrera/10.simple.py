dd973c3f48c1d272e374c3e62e1882d6c55780d3
lidia <lchavezca@unprg.edu.pe>
2019-12-09 03:00:48 -0500

    se agrego 10.simple.py

---
+++ chavez_cabrera/10.simple.py
@@ -0,0 +1,15 @@
+#programa 10
+import os
+#declara
+nombre_del_solicitante=""
+ano_de_inscripcion=0
+#Input via os
+nombre_del_solicitante=(os.sys.argv[1])
+ano_de_inscripcion=int(os.sys.argv[2])
+
+#processing
+#si el numero de hojas supera a 35
+#mostrar "gratis anillado"
+if(ano_de_inscripcion<2030):
+#output
+    print("no pagan inscripcion")
